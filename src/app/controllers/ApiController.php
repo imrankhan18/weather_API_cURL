3f2d2e5b23fa63abab9cd6537eeb8395d8382a87
imrankhan18 <imrankhan@cedcoss.com>
2022-04-07 10:55:44 +0530

    Weather Api With Guzzle

--- src/app/controllers/ApiController.php
+++ src/app/controllers/ApiController.php
@@ -2,9 +2,7 @@
 require '../vendor/autoload.php';
 
 use Phalcon\Mvc\Controller;
-use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Client;
-
 
 class ApiController extends Controller
 {
@@ -28,16 +26,16 @@ class ApiController extends Controller
             $loc = $this->request->getPost('tosearch');
             $loc = urlencode($loc);
             $key = '0bab7dd1bacc418689b143833220304';
-            $url = "http://api.weatherapi.com/v1/search.json?key=$key&q=$loc";
-            // echo $url;
+            $client = new Client(['base_uri' => 'http://api.weatherapi.com/v1/']);
+            $url = "search.json?key=$key&q=$loc";
+
+            $response = $client->request('GET', $url);
+            $r = json_decode($response->getBody(), true);
+            $this->view->message = $r;
+            // echo "<pre>";
+            // print_r($r);
+            // echo "<pre>";
             // die;
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_URL, $url);
-            // curl_setopt($ch, CURLOPT_POST, 1);
-            $curlexec = curl_exec($ch);
-            $location = json_decode($curlexec, true);
-            $this->view->message = $location;
         }
     }
     public function detailsAction()
@@ -46,36 +44,64 @@ class ApiController extends Controller
          * details of cities weather, forecast etc
          */
         if ($this->request->get('alt')) {
-            $aqi = 'yes';
-            $alert = 'yes';
             $action = $this->request->get('action') ?? 'current';
             $alt = $this->request->get('alt');
             if ($action == 'forecast') {
                 $date = 7;
             } elseif ($action == 'history') {
                 $date = '2010-01-01';
-            } elseif ($action == 'wl') {
-                $aqi = 'yes';
-                $alert = 'yes';
             } elseif ($action == 'aqi') {
                 $action = 'forecast';
-                $st = '&aqi=yes';
+                // $st = '&aqi=yes';
             } else {
                 $date = '';
             }
-
+            $st = '&aqi=yes';
             $key = '0bab7dd1bacc418689b143833220304';
-            $url = "http://api.weatherapi.com/v1/" . $action . ".json?key=" . $key . $st . "&q=" . $alt . "&days=" . $date . "&dt=" . $date . "&days=1&aqi=yes&alerts=yes";
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_URL, $url);
-            $curlexec = curl_exec($ch);
-            $val = json_decode($curlexec, true);
+            $client = new Client(['base_uri' => 'http://api.weatherapi.com/v1/']);
+            $url = "". $action . ".json?key=" . $key .$st . "&q=" . $alt . "&days=" . $date . "&dt=" . $date . "&days=1&aqi=yes&alerts=yes";
+            $response = $client->request('GET', $url);
+            $val = json_decode($response->getBody(), true);
+        
             $this->view->data = $val;
-            echo "<pre>";
-            print_r($val);
-            echo "</pre>";
-            // die;
-        }
-    }  
+           
+    }
+    }
+
+    public function postAction()
+    {
+
+        $ch = curl_init();
+        $url = "http://httpbin.org/post";
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt(
+            $ch,
+            CURLOPT_POSTFIELDS,
+            http_build_query(array('postvar1' => '10', 'postvar2' => '20', 'postvar3' => '30'))
+        );
+
+
+        // In real life you should use something like:
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, 
+        //          http_build_query(array('postvar1' => 'value1')));
+
+        // Receive server response ...
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $server_output = curl_exec($ch);
+        $s = json_decode($server_output, true);
+        echo "<pre>";
+        print_r($s);
+        echo "</pre>";
+        curl_close($ch);
+    }
+    public function guzzleAction()
+    {
+       
+        
+        
+    }
 }
